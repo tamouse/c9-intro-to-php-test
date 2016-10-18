a980fab132f02f6c9bf23b19ceac97c62a7a204d
Tamara Temple <tamouse@gmail.com>
2016-10-18 05:30:28 +0000

    initial commit

---
+++ my-first-form.php
@@ -0,0 +1,12 @@
+<h1>Please fill out the form!</h1>
+<form method="POST" action="mailto:tamouse@gmail.com">
+    <fieldset>
+        <label for="name">Name:</label>
+        <input id="name" name="name" type="text_field" value="" placeholder="Enter your name">
+    </fieldset>
+    <fieldset>
+        <label for="comments">Comments:</label>
+        <input type="text" name="comments" id="comments" value="" placeholder="Tell me what you want"/>
+    </fieldset>
+    <input type="submit" name="commit" value="Send note"/>
+</form>
